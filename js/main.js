462e44ff56f61c7a702511cbd578402640a1a994
Andrew Medeiros <amedeiros@clarityservices.com>
2013-04-13 09:19:10 -0400

    Adding some basic GIS API calls

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 var searchValues = {};
+var queryTask, query;
+
+// Setup after dom loaded
+$(function() {
+	dojo.require("esri.tasks.query");
+	dojo.require("esri.map");
+	dojo.ready(init);
+});
 
 function selectedSex() {
 	if ( jQuery("input#search-sex-male").is(':checked') ) {
@@ -11,10 +19,70 @@ function selectedSex() {
 
 function captureForm() {
 	searchValues['breed'] 		= jQuery("input#search-breed").val();
-	searchValues['color'] 		= jQuery("input#search-color").val();
+	searchValues['color'] 		= jQuery("input#search-color").val() || null;
 	searchValues['street']		= jQuery("input#search-street").val();
 	searchValues['city']		= jQuery("input#search-city").val();
-	searchValues['zipCode']		= jQuery("input#search-zipcode").val();
 	searchValues['sex']			= selectedSex();
-	return false;
+	execute();
+	return true;
 }
+
+function init() {
+	//build query
+	queryTask = new esri.tasks.QueryTask("http://54.225.237.62/arcgis/rest/services/Environmental/MapServer/0");
+
+	//build query filter
+	query = new esri.tasks.Query();
+	query.returnGeometry = false;
+	query.outFields = ["Owner","Addr","CITY","City","Breed","Name_and_I","Sex", "Color", "Dd_Tag","Tag_Expire","Tatoo_No","Date_decla","Victim_Inf", "Comment"];
+}
+
+function execute() {
+	query.text = searchValues['color'];
+	query.where = buildQuery();
+	//execute query
+	queryTask.execute(query, showResults);
+}
+
+function buildQuery() {
+	
+	var queryString = "";
+
+	if (searchValues['breed']) {
+		queryString += "Breed LIKE '" + searchValues['breed'] + "'";
+	}
+
+	if (searchValues['color']) {
+		queryString += " Color LIKE '" + searchValues['color'] + "'";
+	}
+
+	if (searchValues['street']) {
+		queryString += " Street LIKE '" + searchValues['street'] + "'";
+	}
+
+	if (searchValues['city']) {
+		queryString += " City LIKE '" + searchValues['city'] + "'";
+	}
+
+	if (searchValues['sex']) {
+		queryString += " Sex LIKE '" + searchValues['sex'] + "'";
+	}
+	alert(queryString);
+	return queryString;
+}
+
+function showResults(results) {
+	var s = "";
+	for (var i=0, il=results.features.length; i<il; i++) {
+	  var featureAttributes = results.features[i].attributes;
+	  for (att in featureAttributes) {
+	    s = s + "<b>" + att + ":</b>  " + featureAttributes[att] + "<br />";
+	  }
+	}
+	alert(s);
+	// jQuery("#info").html(s);
+	// dojo.byId("info").innerHTML = s;
+}
+
+
+
